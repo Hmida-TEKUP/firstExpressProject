9bcb02c1c724906a420bac88454fcd28b7ae20b0
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-25 19:18:02 +0100

    Create Student Table. close #1.
    a

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+// Like a DB
+let students = [
+    {id: 1, name: 'student1', age: 22},
+    {id: 2, name: 'student2', age: 24},
+    {id: 3, name: 'student3', age: 18}
+];
+
