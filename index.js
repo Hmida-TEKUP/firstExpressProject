a16bf244914df2859b91a775071d16eaa5b3c20a
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-25 20:45:36 +0100

    add validator to the code. close #10.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const Joi = require('joi');
 const app = express();
 const port  = process.env.PORT || 3000;
 
@@ -15,15 +15,27 @@ app.get('/api/students', (req,res)=>{
     res.send(students);
 });
 
-app.get('/api/students/id/:iden', (req,res)=>{
-    let student = students.find(s => s.id === parseInt(req.params.iden));
-    if(!student) return res.status(404).send(`Student not found with this id ${req.params.iden}`);
+let path_id_validator = {
+    id : Joi.number().positive().required()
+}
+app.get('/api/students/id/:id', (req,res)=>{
+    let validate_res = Joi.validate(req.params,path_id_validator);
+    if(validate_res.error)
+        return res.status(400).send(validate_res.error.details[0].message);
+    let student = students.find(s => s.id === parseInt(req.params.id));
+    if(!student) return res.status(404).send(`Student not found with this id ${req.params.id}`);
     res.send(student);
 });
 
+let student_validator_schema = {
+    name : Joi.string().min(3).max(12).required(),
+    age : Joi.number().positive().required()
+}
+
 app.post('/api/students', (req,res)=>{
-    if(!req.body.name || !req.body.age)
-        return res.status(400).send('name or age not found');
+    let validate_res = Joi.validate(req.body,student_validator_schema);
+    if(validate_res.error)
+        return res.status(400).send(validate_res.error.details[0].message);
     let student = {
         id: students[students.length-1].id+1,
         name: req.body.name,
