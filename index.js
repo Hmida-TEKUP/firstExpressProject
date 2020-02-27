09841272d2ec173a94205913d060b5ec9d577935
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-27 19:41:11 +0100

    Add PUT of Student. close #8.

--- index.js
+++ index.js
@@ -56,5 +56,18 @@ app.delete('/api/students/id/:id', (req,res)=>{
     res.send(student);
 });
 
+app.put('/api/students/id/:id', (req,res)=>{
+    let validate_res = Joi.validate(req.params,path_id_validator);
+    if(validate_res.error)
+        return res.status(400).send(validate_res.error.details[0].message);
+    let student = students.find(s => s.id === parseInt(req.params.id));
+    if(!student) return res.status(404).send(`Student not found with this id ${req.params.id}`);
+    validate_res = Joi.validate(req.body,student_validator_schema);
+    if(validate_res.error)
+        return res.status(400).send(validate_res.error.details[0].message);
+    student.name = req.body.name;
+    student.age = req.body.age;
+    res.send(student);
+});
 
 app.listen(port, () => console.log(`Listening on ${port}....`));
